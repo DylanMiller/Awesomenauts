a1030dadf883938b408bee7d5f45e1d8c8c23ef8
Dylan Miller <dmiller3199@gmail.com>
2015-01-22 08:55:26 -0800

    video 5

--- js/screens/play.js
+++ js/screens/play.js
@@ -5,7 +5,8 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
-
+                //this will actually load the level instead of just sitting on the loading screen
+                me.levelDirector.loadLevel("level01");
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
