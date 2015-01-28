9a2421829c7fc55d9b586815acdbc53abbcb813d
Dylan Miller <dmiller3199@gmail.com>
2015-01-28 09:15:47 -0800

    video 7 is done

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,5 +10,24 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect( 0, 0, 64, 64)).toPolygon();
                 }
         }]);
+        this.body.setVelocity(5, 0);
+    },
+    update: function(delta){
+        if(me.input.isKeyPressed("right")) {
+            //sets the position of x with maths
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        }else{
+            this.body.vel.x = 0;
+        
+      }
+            this.body.update(delta);
+            return true;
+            if(me.input.isKeyPressed("left")) {
+            //sets the position of x with maths
+            this.body.vel.x -= this.body.accel.x / me.timer.tick;
+        }
     }
+        
+      
+    
 });
--- js/screens/play.js
+++ js/screens/play.js
@@ -11,7 +11,12 @@ game.PlayScreen = me.ScreenObject.extend({
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
                 
-                me.input.bindKey(me.input.KEY.RIGHT);
+                var player = me.pool.pull("player", 0, 420, {});
+                
+                me.game.world.addChild(player, 5);
+                
+                me.input.bindKey(me.input.KEY.RIGHT, "right");
+                me.input.bindKey(me.input.KEY.LEFT, "left");
 	},
 
 
