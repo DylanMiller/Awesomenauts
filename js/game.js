d8c670fa7548dd1a9323a82acb93f8e0151e09e4
dylanmiller <dmiller3199@gmail.com>
2015-02-25 09:12:42 -0800

    video 19

--- js/game.js
+++ js/game.js
@@ -43,7 +43,7 @@ var game = {
                 me.pool.register("PlayerBase", game.PlayerBaseEntity);
                 me.pool.register("EnemyBase", game.EnemyBaseEntity);
                 me.pool.register("EnemyCreep", game.EnemyCreepEntity, true);
-                
+                 me.pool.register("GameManager", game.GameManager);
                 var player = me.pool.pull("player", 0, 420, {});
                 me.game.world.addChild(player, 5);
             
