7ae09dc91ecf5afa885fd3c202a6c869b75520bc
dylanmiller <dmiller3199@gmail.com>
2015-03-27 08:22:12 -0700

    video 30

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,15 +1,8 @@
 game.PlayerEntity = me.Entity.extend({
     init: function( x, y, settings) {
-        this._super(me.Entity, 'init', [x, y, {
-                image: "player",
-                width: 64,
-                height: 64,
-                spritewidth: "64",
-                spriteheight: "64",
-                getShape: function(){
-                    return(new me.Rect( 0, 0, 64, 64)).toPolygon();
-                }
-        }]);
+        this.setSuper();
+        this.setPlayerTimers();
+        
         this.type = "PlayerEntity";
         this.health = game.data.playerHealth;  
         this.body.setVelocity(game.data.playerMoveSpeed, 20);
@@ -24,6 +17,24 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.addAnimation("attack", [66, 67, 68, 69, 70, 71, 72], 80);
         this.renderable.setCurrentAnimation("idle");
     },
+    
+    setSuper: function(){
+        this._super(me.Entity, 'init', [x, y, {
+                image: "player",
+                width: 64,
+                height: 64,
+                spritewidth: "64",
+                spriteheight: "64",
+                getShape: function(){
+                    return(new me.Rect( 0, 0, 64, 64)).toPolygon();
+                }
+        }]);
+    },
+    
+    setPlayerTimers: function(){
+        
+    },
+    
     update: function(delta){
         this.now = new Date().getTime();
         if (this.health <= 0) {
